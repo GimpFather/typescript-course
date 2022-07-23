099449b5b1659914c627febc6ad858200a0c4dce
Filip Gałczyk <filip.galczyk@waynet.pl>
2022-07-23 15:14:43 +0200

    clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import NewTodos from "./components/NewTodos";
-import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import "./components/css/App.css";
 
 function App() {
-	const todos = [new Todo("Learn React"), new Todo("Learn TS")];
-	const addTodoHandler = (todoText: string) => {};
 	return (
-		<div className="App">
-			<NewTodos onAddTodo={addTodoHandler}></NewTodos>
-			<Todos items={todos}></Todos>
+		<div>
+			<p>Hello there!</p>
 		</div>
 	);
 }
--- src/components/TodosItem.tsx
+++
@@ -1,5 +0,0 @@
-const TodosItem: React.FC<{ text: string }> = (props) => {
-	return <li>{props.text}</li>;
-};
-
-export default TodosItem;
